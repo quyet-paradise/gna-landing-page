c52cdebe273acbcaaa89666d440143aa9c088d17
Quyet Tran <quyet.trantien0402@gmail.com>
2025-04-28 16:21:03 +0700

    - fix hotline

--- src/components/footer/Footer.vue
+++ src/components/footer/Footer.vue
@@ -4,7 +4,14 @@
             <div class="footer__left">
                 <h2 class="company-name">ThicongdenLED247</h2>
                 <div class="subtitle">Mang đến giải pháp toàn diện, tiết kiệm và thẩm mỹ cho mọi không gian</div>
-                <div class="hotline"><b>Hotline / Zalo:</b> +84 33 293 7600</div>
+                <div class="hotline">
+                    <p><b>Hotline / Zalo:</b></p>
+                    <ul>
+                        <li>0865077247: Liên hệ tư vấn - báo giá thi công nội thất & đèn LED</li>
+                        <li>0947931247: Liên hệ bộ phận phân phối đại lí cấp 1 đèn LED</li>
+                    </ul>
+                </div>
+                
                 <div class="email"><b>Email:</b> chuyenthicongdenled247@gmail.com</div>
 
                 <div class="contact">
