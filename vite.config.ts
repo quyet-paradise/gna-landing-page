5040fc6b3a0eb0ee229912f56ff9e2e1a2428b62
Quyet Tran <quyet.trantien0402@gmail.com>
2026-03-04 11:30:30 +0700

    - [GNA] update sitemap and meta description tag for SEO Ads

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,20 @@
 import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Sitemap from 'vite-plugin-sitemap'
+import routes from '@/router/app-route'
 
 const pathSrc = path.resolve(__dirname, 'src')
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Sitemap({
+      hostname: `https://www.thicongdenled247.com`,
+      dynamicRoutes: routes.map((route: { path: any }) => route.path)
+    })
+  ],
   resolve: {
     alias: {
       '@/': `${pathSrc}/`,
