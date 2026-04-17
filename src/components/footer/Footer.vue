5405ae2bd5e04456aa10e4e536e83961b47b7f30
Quyet Tran <quyet.trantien0402@gmail.com>
2026-04-17 09:47:14 +0700

    [GNA] fix feedbacks

--- src/components/footer/Footer.vue
+++ src/components/footer/Footer.vue
@@ -26,7 +26,7 @@
                     </a>
                 </div>
             </div>
-            <div class="footer__center">
+            <!-- <div class="footer__center">
                 <h2 class="address">Địa chỉ</h2>
                 <div class="address-sub"><b>Văn phòng:</b> 40 P. Ngụy Như Kon Tum, Nhân Chính, Thanh Xuân, Hà Nội</div>
                 <div class="address-sub"><b>Showroom:</b> 26 Nguyễn Xiển - Thanh Xuân - Hà Nội</div>
@@ -35,7 +35,7 @@
                     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1862.429154899475!2d105.79745704837006!3d20.998316086775674!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135acbc89f2aa1b%3A0xd5b7b6aa22d84ecf!2zNDAgUC4gTmfhu6V5IE5oxrAgS29uIFR1bSwgTmjDom4gQ2jDrW5oLCBUaGFuaCBYdcOibiwgSMOgIE7hu5lpLCBWaeG7h3QgTmFt!5e0!3m2!1svi!2s!4v1744822064106!5m2!1svi!2s" width="600" height="450" style="border:0;" allowfullscreen="true" loading="lazy" referrerpolicy="no-referrer-when-downgrade">                        
                     </iframe>
                 </div>
-            </div>
+            </div> -->
 
             <div class="footer__right">
                 <h2 class="title">DỊCH VỤ NỔI BẬT</h2>
@@ -45,7 +45,7 @@
             </div>
         </div>
 
-        <div class="copyright">© 2025 <b>ThicongdenLED247</b></div>
+        <div class="copyright">© {{ new Date().getFullYear() }} <b>ThicongdenLED247</b></div>
     </div>
 </template>
 
